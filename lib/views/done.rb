c564202dbfb0b5abc16085744fbfc8804af56078
stug43 <nganta.a@hotmail.fr>
2019-01-22 23:59:20 +0100

    dernières modifications

--- lib/views/done.rb
+++ lib/views/done.rb
@@ -1,11 +1,13 @@
 class Done
 	
 	def its_over_method
+		puts
 		2.times {puts("\t"+"#"*53)}
 		2.times {puts("\t"+"#"+" "*51+"#")}
 		puts("\t"+"#"+" "*21+"IT'S DONE"+" "*21+"#")
 		2.times {puts("\t"+"#"+" "*51+"#")}
 		puts("\t"+"#"*53)
+		puts
 	end
 
 	def initialize
